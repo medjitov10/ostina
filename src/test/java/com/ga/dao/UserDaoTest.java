ce711e73afb113a3a59cfb517442f1aa2ee006d7
Hristina Lapanova <hristina.lapanova@gmail.com>
2019-10-25 15:53:30 -0400

    added userDao signup test

--- src/test/java/com/ga/dao/UserDaoTest.java
+++ src/test/java/com/ga/dao/UserDaoTest.java
@@ -6,11 +6,15 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.junit.Assert;
 import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -58,13 +62,26 @@ public class UserDaoTest {
     Query<User> query;
     
     @Test
-    public void getuserByUserName_User_Success() {
+    public void getuserByUserName_UserDao_Success() {
     	when(session.createQuery(anyString())).thenReturn(query);
     	when(query.getSingleResult()).thenReturn(user);
     	User userTemp = userDao.getUserByUserName(user.getUsername());
     	assertEquals(user, userTemp);
     	
     }
+    @Test
+    public void listUsers_UserDao_Success() {
+    	when(session.createQuery(anyString())).thenReturn(query);
+    	List<User> users = userDao.listUsers();
+    	Assert.assertNotNull(users);
+    	
+    }
+    @Test
+    public void signUp_UserDao_Success() {
+    	User userTemp = userDao.signUp(user);
+    	assertEquals(userTemp, user);
+    }
+    
 	
 
 }
